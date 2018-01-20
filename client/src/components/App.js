681993d1d324a43b6b72a1eaf7e05f27bc9850a8
yymin <yyming009@gmail.com>
2018-01-20 12:11:36 -0800

    almost final production

---
+++ client/src/components/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
+import Header from './Header';
+import Landing from './Landing';
+import Dashboard from './Dashboard';
+import SurveyNew from './surveys/SurveyNew';
+
+
+
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact={true} path="/" component={Landing} />
+            <Route exact={true} path="/surveys" component={Dashboard} />
+            <Route exact={true} path="/surveys/new" component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default connect(null, actions)(App);
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
-const mongoose = require('mongoose');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
 const User = mongoose.model('users');
@@ -12,7 +12,7 @@ passport.serializeUser((user, done) => {
 passport.deserializeUser((id, done) => {
   User.findById(id).then(user => {
     done(null, user);
-  })
+  });
 });
 
 passport.use(
@@ -23,20 +23,19 @@ passport.use(
       callbackURL: "/auth/google/callback",
       proxy: true
     },
-    (accessToken,refreshToken, profile, done) => {
+    async (accessToken,refreshToken, profile, done) => {
       // console.log('access Token: ', accessToken);
       // console.log('refresh Token: ', refreshToken);
       // console.log('profile: ', profile);
-      User.findOne({  googleId: profile.id }).then((existingUser) => {
-          if (existingUser) {
-            //we already have a record with the given profile ID
-            done(null, existingUser);
-          } else {
-            //we don't have a user record with this ID, make a new record
-            new User({ googleId: profile.id }).save().then(user => done(null, user));
-          }
-        })
+      const existingUser = await User.findOne({ googleId: profile.id })
 
+      if (existingUser) {
+        //we already have a record with the given profile ID
+        return done(null, existingUser);
+      }
+      //we don't have a user record with this ID, make a new record
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
     }
   )
 );
